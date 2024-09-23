23444858312ebbc97054b95e578f61d6964dc8a1
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2024-09-23 16:50:23 +0530

    Update Printing from 1 to 10.py

--- Printing from 1 to 10.py
+++ Printing from 1 to 10.py
@@ -1,9 +1,10 @@
 def Priting_From_1_To_10(n):
-    if (n > 10):
+    if (n < 1):
         return
     
+    Priting_From_1_To_10(n - 1)
+
     print(n)
 
-    Priting_From_1_To_10(n + 1)
 
-Priting_From_1_To_10(0)
+Priting_From_1_To_10(10)
