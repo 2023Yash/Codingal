1ad3b9be355e11788f98c89d047b7b088a0d7aae
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2024-10-04 16:38:29 +0530

    Create ().py

---
+++ ().py
@@ -0,0 +1,19 @@
+def parantheses(s, l, r, p, n):
+    if p == 2 * n:
+        for i in s:
+            print(i, end="")
+        print("\n")
+    
+    if (l > r):
+        s[p] = "}"
+        parantheses(s, l, r + 1, p + 1, n)
+
+    if (l < n):
+        s[p] = "{"
+        parantheses(s, l + 1, r, p + 1, n)
+
+n = int(input("Enter a number:"))
+
+s = [""] * 2 * n
+
+parantheses(s, 0, 0, 0, n)
