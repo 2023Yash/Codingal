cd581be1430ce2982478aafa3ed890346f5417d5
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2024-10-07 16:00:42 +0530

    Update

--- ().py
+++ ().py
@@ -1,19 +1,23 @@
-def parantheses(s, l, r, p, n):
-    if p == 2 * n:
-        for i in s:
-            print(i, end="")
-        print("\n")
+# def parantheses(s, l, r, p, n):
+#     if p == 2 * n:
+#         for i in s:
+#             print(i, end="")
+#         print("\n")
     
-    if (l > r):
-        s[p] = "}"
-        parantheses(s, l, r + 1, p + 1, n)
+#     if (l > r):
+#         s[p] = "}"
+#         parantheses(s, l, r + 1, p + 1, n)
 
-    if (l < n):
-        s[p] = "{"
-        parantheses(s, l + 1, r, p + 1, n)
+#     if (l < n):
+#         s[p] = "{"
+#         parantheses(s, l + 1, r, p + 1, n)
 
-n = int(input("Enter a number:"))
+# n = int(input("Enter a number:"))
 
-s = [""] * 2 * n
+# s = [""] * 2 * n
 
-parantheses(s, 0, 0, 0, n)
+# parantheses(s, 0, 0, 0, n)
+
+s = "I like to CODE!"
+
+print(s[7:3:-3])
