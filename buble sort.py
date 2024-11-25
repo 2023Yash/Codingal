771ca34219ddc801dda7dcc35fde7a899b32f5cd
Yash Naudiyal <yashnaudiyal268@gmail.com>
2024-11-25 17:12:01 +0530

    Update buble sort.py

--- buble sort.py
+++ buble sort.py
@@ -20,3 +20,23 @@ def bubble_sort(arr):
 arr = [5, 2, 7, 8, 2, 6, 4]
 
 bubble_sort(arr)
+
+def varry_varry_efficiaant_bubble_sort(arr):
+    arr_len = len(arr)
+
+    for i in range(arr_len):
+        swap = False
+
+        for j in range(0, arr_len - 1):
+            if(arr[i] > arr[i + 1]):
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap = True
+
+        if (swap == False):
+            break
+
+    print(arr)
+
+arr2 = [4, 2, 9, 8, 2, 4]
+
+varry_varry_efficiaant_bubble_sort(arr)
