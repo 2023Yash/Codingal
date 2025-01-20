61d73c3e92d0fee4d43f1cbfa05df289966419e5
Yash Naudiyal <yashnaudiyal268@gmail.com>
2025-01-20 17:25:48 +0530

    ipdate

--- reversing string.py
+++
@@ -1,9 +0,0 @@
-def rev_string(string):
-    li = list(string)
-    length = len(li)
-    for i in range(length // 2):
-        li[i], li[length - i - 1] = li[length - i - 1], li[i]
-    return "".join(li)
-
-string = "Yash Naudiyal"
-print(rev_string(string))
