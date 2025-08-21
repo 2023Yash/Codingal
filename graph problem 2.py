279505ed614c25bf671d52a7a5f542d3e1fa6cde
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2025-08-21 17:20:33 +0530

    Create graph problem 2.py

---
+++ graph problem 2.py
@@ -0,0 +1,22 @@
+def is_sum_string_util(s, start, n1, n2):
+    if start == len(s):
+        return True
+    total = str(n1 + n2)
+    l = len(total)
+    if start + l > len(s) or s[start:start+l] != total:
+        return False
+
+def is_sum_string(s):
+    n = len(s)
+
+    for i in range(1, n):
+        for j in range(i+1, n):
+            n1 = int(s[:i])
+            n2 = int(s[i:j])
+            if is_sum_string_util(s, j, n1, n2):
+                return True
+    return False
+
+print(is_sum_string("122436"))
+print(is_sum_string("1111112223"))
+print(is_sum_string("1235"))
