63dffd18ebcabceb80cc5a63d6e8077397b5e50b
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2025-12-11 18:02:10 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,21 @@
+# recution
+def factorial(n):
+    if n == 0:
+        return 1
+    else: 
+        return n * factorial(n - 1)
+
+print(factorial(5))
+
+# dynamic
+
+def dyn_factorial(n):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        dp[i] = i * dp[i - 1]
+    
+    return dp[n]
+
+print(dyn_factorial(5))
