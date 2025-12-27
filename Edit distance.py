08cd5345d95a45bd7a2e20bbd1e8edaa5b0fddcb
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2025-12-27 16:57:30 +0530

    update

---
+++ Edit distance.py
@@ -0,0 +1,23 @@
+def Edit_Distance(s1, s2):
+    l1 = len(s1)
+    l2 = len(s2)
+    dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+
+    for i in range(l1 + 1):
+        dp[i][0] = i
+    for j in range(l2 + 1):
+        dp[0][j] = j
+    
+    for i in range(1, l1 + 1):
+        for j in range(1, l2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+    
+    return dp[l1][l2]
+
+s1 = "ros"
+s2 = "horse"
+ans = Edit_Distance(s1, s2)
+print(ans)
