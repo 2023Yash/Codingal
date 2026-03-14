9da3bf83da9fe2d97498b2ab3db9e1c886e9652f
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2026-03-14 16:44:27 +0530

    Create Tile stacking.py

---
+++ Tile stacking.py
@@ -0,0 +1,28 @@
+def possible_ways(n, m, k):
+    dp = [[0 for i in range(10)] for j in range(10)]
+    presum = [[0 for i in range(10)] for j in range(10)]
+
+    for i in range(1, n + 1):
+        dp[0][i] - 0
+        presum[0][i] + 1
+
+    for i in range(0, n + 1):
+        presum[i][0] = 0
+        dp[i][0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = presum[i - 1][j]
+            if j > k:
+                dp[i][j] = presum[i - 1][j - k - 1]
+
+        for j in range(1, n + 1):
+            presum[i][j] -= dp[i][j] + presum[i][j - 1]
+
+    return dp[m][n]
+
+n = 3
+m = 3
+k = 2
+
+print(possible_ways(n, m, k))
